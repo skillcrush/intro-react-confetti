91f7fffce704a25d525090d4c3852361d3a31a0c
Ann <4411121+redrambles@users.noreply.github.com>
2023-03-03 14:49:39 -0500

    Remove unecessary files and install confetti

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Confetti from "react-confetti";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Confetti />
+      <h1>Wow, I'm working with React on my machine!</h1>
+
+      <p>Is this what happiness feels like?</p>
     </div>
   );
 }
